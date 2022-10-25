a36ef32c363a98b9dc550198b3832bcc5e54b0c9
tuan <rickytuan1991@gmail.com>
2022-10-25 11:49:26 +0300

    push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './card/Card'
+const persons = [
+  {
+    name: 'Margit',
+    title: 'CEO',
+    age: 29,
+    languages: [
+      {
+        name: 'english', id: 1
+      },
+      {
+        name: 'finnish', id: 2
+      },
+      {
+        name: 'russian', id: 3
+      },
+    ],
+  },
+  {
+    name: 'Kati',
+    title: 'Developer',
+    age: 25,
+    languages: [
+      {
+        name: 'english', id: 1
+      },
+      {
+        name: 'finnish', id: 2
+      },
+      {
+        name: 'russian', id: 3
+      },
+      {
+        name: 'spanish', id: 4
+      },
+    ],
+  },
+  {
+    name: 'Karin',
+    title: 'Designer',
+    age: 45,
+    languages: [
+      {
+        name: 'english', id: 1
+      },
+      {
+        name: 'russian', id: 2
+      },
+    ],
+  },
+];
 
+const createBoxs = () => {
+  return persons.map((item, index) => {
+    return <Card key={index} person={item}></Card>
+  })
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className='header'>
+        this is my header
       </header>
-    </div>
+      <div className='main' >
+        {createBoxs()}
+      </div>
+    </>
   );
 }
 
---
+++ src/card/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import style from "./card.module.css";
+export default function card(props) {
+  const { name, title, age, languages } = props.person;
+  return (
+    <div className={style["box"]}>
+      <p>Name : {name}</p>
+      <p>Title : {title}</p>
+      <p>age : {age}</p>
+      {languages.map((item, index) => {
+        const { name, id } = item;
+        return <span key={index}>name: {name}</span>;
+      })}
+    </div>
+  );
+}
