e9b87333f563b6b5827938e9ad770e53af0c85a8
tuan <rickytuan1991@gmail.com>
2022-10-26 12:40:11 +0300

    done

--- src/counter/count_button/Count.jsx
+++ src/counter/count_button/Count.jsx
@@ -1,13 +1,3 @@
-// import React, { Component } from "react";
-
-// export default class Count extends Component {
-
-//   render() {
-//     let { name, fn, value } = this.props.value;
-
-//     return <div className="card bg">{name}</div>;
-//   }
-// }
 import style from "./Count.module.css";
 const Count = (props) => {
   let { name, value } = props.value;
